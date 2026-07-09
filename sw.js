e018a468044374e76f1d85bfb83064735f84c8ff
guilhermesimoni6 <guilhermesimoni6@gmail.com>
2026-07-09 17:46:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "atividades-shell-v78";
+const CACHE_NAME = "atividades-shell-v80";
 const SHELL_FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL_FILES))); self.skipWaiting(); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
